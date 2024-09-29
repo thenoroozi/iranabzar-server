26e25fee19f7b2f44d00829dfa54a80cdcf24a3b
Hamed Noroozi <noruzi206@gmail.com>
2024-09-29 19:37:12 +0330

    update index.js

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ const port = process.env.PORT || 8000; //  chose port from here like 8080, 3001
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port,()=>{
+   console.log("JSON server is running..."); 
+});
